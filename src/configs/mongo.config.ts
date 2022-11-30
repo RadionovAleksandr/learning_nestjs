ad203b809efbcc356248333505616d160e25c65c
AlexR <radionovAlek@yandex.ru>
2022-11-30 21:22:29 +0700

    create docker-compose, tslint and mongo config

---
+++ src/configs/mongo.config.ts
@@ -0,0 +1,23 @@
+import { ConfigService } from "@nestjs/config";
+import { TypegooseModuleOptions } from "nestjs-typegoose";
+
+export const getMongoConfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => {
+  return {
+    uri: getMongoString(configService),
+    ...getMongoOptions()
+  };
+};
+
+const getMongoString = (configService: ConfigService) =>
+  'mongodb://' +
+  configService.get('MONGO_LOGIN') +
+  ':' +
+  configService.get('MONGO_PASSWORD') +
+  '@' +
+  configService.get('MONGO_HOST') +
+  ':' +
+  configService.get('MONGO_PORT') +
+  '/' +
+  configService.get('MONGO_AUTH_DB');
+
+const getMongoOptions = () => ({});
